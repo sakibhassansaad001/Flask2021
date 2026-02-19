dda91403569ec4ec39f47ddce6e9e10377c7867f
sakibhassansaad001 <shakibshadshs00@gmail.com>
2026-02-19 15:49:34 +0600

    Initial commit

--- app.py
+++ app.py
@@ -4,8 +4,8 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-   return render_template('index.html')
+   return render_template('base.html')
     # return "<p>Hello, World!</p>"
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run()
